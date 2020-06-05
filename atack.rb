6b55ebc4e91caee92a2a7ce40249e17468c8bc99
paulor8bit <thunder_momon@hotmail.com>
2020-06-04 22:37:21 -0300

    Update atack.rb
    Atualização com funções

--- atack.rb
+++ atack.rb
@@ -1,38 +1,101 @@
-
 h = 2
 q = 1
 e = 0.5
 s = 0.25
-live_loop :synth2 do
-  use_synth :tb303
-  use_synth_defaults sustain: q+e, release: q
-  play (ring :d2,:e2,:c2,:a1).tick
-  sleep h
-end
 
-live_loop :flibble do
-  sample :bd_haus, rate: 1
-  sleep 0.5
-end
-
-
-with_fx :reverb do
-  live_loop :synth1 do
-    tick
-    use_synth :saw
-    play (knit :d3,4,:e3,4,:c3,4,:a3,4).look
-    sleep (ring e+s,e,s,e).look
+pausaPassagemUm=-1
+define :passagemUm do |root, repeats|
+  repeats.times do
+    in_thread do
+      loop do
+        
+        if (pausaPassagemUm<0)
+          #Começo Repetição
+          use_synth :tb303
+          use_synth_defaults sustain: q+e, release: q
+          play (ring :d2,:e2,:c2,:a1).tick
+          sleep h
+          #Fim Repetição
+          
+        else
+          stop
+          
+        end
+        
+      end
+    end
+    
   end
 end
 
-blast = (ring 1,1,1,1,1,1,1,1)
-kik = (ring 1,0,0,0,1,0,0,0)
-
-#Beat:
-live_loop :drums do
-  tick
-  sample :sn_dolf if (ring 1,1,0,1,1,0).look == 1
-  sample :drum_cymbal_open if (ring 0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0).look == 1
-  sleep 0.25
+pausaPassagemDois=-1
+define :passagemDois do |root, repeats|
+  repeats.times do
+    in_thread do
+      loop do
+        
+        if (pausaPassagemDois<0)
+          #Começo Repetição
+          sample :bd_haus, rate: 1
+          sleep 0.5
+          #Fim Repetição
+          
+        else
+          stop
+          
+        end
+        
+      end
+    end
+    
+  end
 end
 
+pausaPassagemQuatro=-1
+define :passagemQuatro do |root, repeats|
+  repeats.times do
+    in_thread do
+      loop do
+        
+        if (pausaPassagemQuatro<0 )
+          #Começo Repetição
+          tick
+          sample :sn_dolf if (ring 1,1,0,1,1,0).look == 1
+          sample :drum_cymbal_open if (ring 0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0).look == 1
+          sleep 0.25
+          #Fim Repetição
+          
+        else
+          stop
+          
+        end
+        
+      end
+    end
+    
+  end
+end
+
+
+
+passagemUm :e3, 1
+sleep 5
+passagemDois :e3, 1
+sleep 5
+passagemTres :e3, 1
+sleep 5
+pausaPassagemUm=1
+passagemQuatro :e3, 1
+sleep 2
+pausaPassagemQuatro=1
+sleep 2
+pausaPassagemUm=-1
+pausaPassagemQuatro=-1
+passagemUm :e3, 1
+passagemQuatro :e3, 1
+sleep 7
+pausaPassagemQuatro=1
+pausaPassagemDois=1
+pausaPassagemUm=1
+sleep 2
+pausaPassagemTres=1

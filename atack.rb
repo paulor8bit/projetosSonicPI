1a514bef3894bb129c153cfdffbc200cf689972c
paulor8bit <thunder_momon@hotmail.com>
2020-06-04 22:56:27 -0300

    update atack

--- atack.rb
+++ atack.rb
@@ -102,25 +102,59 @@ define :passagemQuatro do |root, repeats|
 end
 
 
+pausaPassagemCinco=-1
+p_kik = (ring 1,0,0,1,0,3,0,0)
+p_sna = (ring 0,0,1,0,0,0,1,0)
+p_hat = (ring 1,1,5,5,3,4,5,3)
+fill = 0
+f_tom = (ring 4,4,4,4,4,4,4,4)
+f_kik = (ring 0,3,3,0,3,0,4,4)
+f_sna = (ring 5,3,0,5,6,7,0,5)
+define :passagemCinco do |root, repeats|
+  repeats.times do
+    in_thread do
+      loop do
+        
+        if (pausaPassagemCinco<0 )
+          #Começo Repetição
+          tick
+          sample :drum_bass_hard if one_in(p_kik.look)
+          sample :drum_snare_hard if one_in(p_sna.look)
+          sample (knit :drum_cymbal_closed,5, :drum_cymbal_pedal,1).choose if one_in(p_hat.look)
+          
+          sleep 0.25
+          #Fim Repetição
+          
+        else
+          stop
+          
+        end
+        
+      end
+    end
+    
+  end
+end
+
+
 
 passagemUm :e3, 1
-sleep 5
+sleep 7
 passagemDois :e3, 1
 sleep 5
 passagemTres :e3, 1
 sleep 5
-pausaPassagemUm=1
 passagemQuatro :e3, 1
 sleep 2
 pausaPassagemQuatro=1
 sleep 2
-pausaPassagemUm=-1
 pausaPassagemQuatro=-1
-passagemUm :e3, 1
-passagemQuatro :e3, 1
-sleep 7
+passagemCinco :e3, 1
+sleep 25
 pausaPassagemQuatro=1
 pausaPassagemDois=1
+pausaPassagemCinco=1
+sleep 7
 pausaPassagemUm=1
-sleep 2
+sleep 3
 pausaPassagemTres=1
